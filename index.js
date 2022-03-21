d123d7c8fdd1d5696e88dbe019b475f76a8b28dd
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-21 14:06:26 -0400

    commit tree 6d2a5eba61cb7fa02ea9d446252ab0aea36b825a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function addEventListener() {
+}
+
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4"
+
+ document.addEventListener("keydown", function(event) {
+        if (event.key === "ArrowLeft") {
+          moveDodgerLeft();   
+        }
+        else if (event.key === "ArrowRight") {
+            moveDodgerRight();
+        }  
+    })
+    
+    function moveDodgerLeft() {
+            const leftNumbers = dodger.style.left.replace("px", "")
+            const left = parseInt(leftNumbers, 10)
+
+            if (left > 0) {
+                dodger.style.left = `${left - 10}px`
+            }
+        }
+
+        function moveDodgerRight() {
+                const rightNumbers = dodger.style.left.replace("px", "")
+                const right = parseInt(rightNumbers, 10)
+    
+                if (right < 360)
+                dodger.style.left = `${right + 10}px` 
+            }  
